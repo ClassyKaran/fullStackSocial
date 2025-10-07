524b653ba7d50627a1c9dd93efe9e3259de45ff8
KARAN RAJPUT <118068866+ClassyKaran@users.noreply.github.com>
2025-10-07 13:53:26 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+  
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  plugins: [react()],
-  
-})
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/_redirects', // tumhara _redirects file path
+          dest: '.',                 // copy to root of dist/
+        },
+      ],
+    }),
+  ],
+});
