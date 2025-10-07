8ab56a800223d71b84d1ba09e0b26f13dc417d8e
Karan Rajput <r.karan21011@gmail.com>
2025-10-07 10:57:58 +0530

    last time udate

--- Backend/app.js
+++ Backend/app.js
@@ -11,23 +11,39 @@ const { errorHandler } = require("./middlewares/errorHandler");
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server, { cors: { origin: "*" } });
 
+
+// const io = socketio(server, { cors: { origin: "*" } });
+
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
+
+
+const io = socketio(server, {
+  cors: {
+    origin: process.env.FRONTEND_URL, // from .env
+    credentials: true,
+  },
+});
+
+// ✅ CORS config using env
 app.use(
   cors({
-    origin: "http://localhost:5173", // Specify frontend origin, not '*'
+    origin: process.env.FRONTEND_URL, // FRONTEND_URL from .env
     credentials: true,
   })
 );
+
 
 app.use(express.json());
 // Serve uploads folder as static
 app.use("/uploads", express.static(require("path").join(__dirname, "uploads")));
 
-// Routes
-// app.use('/', (req, res) => {
-//   res.send('API is running...');
-// });
+
 
 app.use("/auth", authRoutes);
 app.use("/posts", postRoutes);
